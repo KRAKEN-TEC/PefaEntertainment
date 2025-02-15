29247fb81b090e9b3141589218927285757decfb
Fezi-x <tzayamin@gmail.com>
2025-02-16 06:41:19 +0700

    refactor css

--- frontend/src/components/Movies.tsx
+++ frontend/src/components/Movies.tsx
@@ -1,37 +1,36 @@
-import { useMovie } from '@/hooks/useMovie'
-import './CSS/Movies.css'
+import { useMovie } from "@/hooks/useMovie";
+import "./CSS/Movies.css";
 
-export default function Movies(){
-   const {data: movies } = useMovie();
-   const navigate = () =>{
-    console.log("Hello")
-   }
-    return(
-        <div  className="movie-section">
-            <h2>Movies</h2>
-            <span onClick={()=>navigate()}>See more</span>
-            <div className="scroll-container" >
-
-            <div className="movie-grid">
-
-            { movies && movies.filter(m => !m.isSerie).map((movie) =>
-                     <div className="movie-box"  key={movie._id} > 
-                        <img src={movie.poster_url} />
-                        <div>
-                            <h3>{movie.title}</h3>
-                            <span>{movie.description}</span>
-                            <ul>
-                                    {movie.genres.map((g)=>(
-                                        <li key={g._id}>{g.name}</li>
-                                    ))}
-                            </ul> 
-                        </div>
-
-                   </div>
-           )}
-           </div>
-                 </div> 
-            
+export default function Movies() {
+  const { data: movies } = useMovie();
+  const navigate = () => {
+    console.log("Hello");
+  };
+  return (
+    <div className="movie-section">
+      <h2>Movies</h2>
+      <span onClick={() => navigate()}>See more</span>
+      <div className="scroll-container">
+        <div className="movie-grid">
+          {movies &&
+            movies
+              .filter((m) => !m.isSerie)
+              .map((movie) => (
+                <div className="movie-box" key={movie._id}>
+                  <img src={movie.poster_url} />
+                  <div className="movie-text">
+                    <h3>{movie.title}</h3>
+                    <span>{movie.description}</span>
+                    <ul>
+                      {movie.genres.map((g) => (
+                        <li key={g._id}>{g.name}</li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/Series.tsx
+++ frontend/src/components/Series.tsx
@@ -1,39 +1,41 @@
-import { useMovie } from '@/hooks/useMovie'
-import './CSS/Series.css'
+import { useMovie } from "@/hooks/useMovie";
+import "./CSS/Series.css";
 
-export default function Series(){
-   const {data: series } = useMovie();
+export default function Series() {
+  const { data: series } = useMovie();
 
-   const navigate = () =>{
-    console.log("Hello")
-   }
+  const navigate = () => {
+    console.log("Hello");
+  };
 
-    return(
-        <div  className="series-section">
-            <h2>Series</h2>
-            <span onClick={()=>navigate()}>See more</span>
-            <div className="series-scroll-container" >
-
-            <div className="series-grid">
-
-            { series && series.filter(s => s.isSerie).slice(0,3).map((s) =>
-                     <div className="series-box"  key={s._id} > 
-                        <img src={s.poster_url} />
-                        <div>
-                            <h3>{s.title}</h3>
-                            <span>{s.description}</span>
-                            <ul>
-                                    {s.genres.map((g)=>(
-                                        <li key={g._id}>{g.name}</li>
-                                    ))}
-                            </ul> 
-                        </div>
-
-                   </div>
-           )}
-           </div>
-                 </div> 
-            
+  return (
+    <div className="series-section">
+      <h2>Series</h2>
+      <span onClick={() => navigate()}>See more</span>
+      <div className="series-scroll-container">
+        <div className="series-grid">
+          {series &&
+            series
+              .filter((s) => s.isSerie)
+              .slice(0, 3)
+              .map((s) => (
+                <div className="series-box" key={s._id}>
+                  <img src={s.poster_url} />
+                  <div>
+                    <div className="series-text">
+                      <h3>{s.title}</h3>
+                      <span>{s.description}</span>
+                      <ul>
+                        {s.genres.map((g) => (
+                          <li key={g._id}>{g.name}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
