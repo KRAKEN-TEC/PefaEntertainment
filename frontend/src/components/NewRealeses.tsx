8a112f0b984dc5ebfe7fddaba217360124bae9d3
ChristianChann <141214253+ChristianChann@users.noreply.github.com>
2025-02-15 22:54:17 +0700

    Add - NewRealeses

--- frontend/src/components/NewRealeses.tsx
+++ frontend/src/components/NewRealeses.tsx
@@ -1,17 +1,33 @@
+import { useMovie } from '@/hooks/useMovie'
 import './CSS/NewRealses.css'
 
 export default function NewRealeses(){
-
+   const {data: movies } = useMovie();
     return(
         <div  className="movie-section">
             <h2>New Realsese</h2>
-            <div className="scroll-container">
-               <div className="movie-box">
-                 <img src="pic1.jpg" alt="Movie 1" />
-                 <p>Movie 01</p>
-               </div>
-            </div>
+            <div className="scroll-container" >
 
+            <div className="movie-grid">
+
+            { movies && movies.map((movie) =>
+                     <div className="movie-box"  key={movie._id} > 
+                        <img src={movie.poster_url} />
+                        <div>
+                            <h3>{movie.title}</h3>
+                            <span>{movie.description}</span>
+                            <ul>
+                                    {movie.genre.map((g)=>(
+                                        <li key={g._id}>{g.name}</li>
+                                    ))}
+                            </ul> 
+                        </div>
+
+                   </div>
+           )}
+           </div>
+                 </div> 
+            
         </div>
     )
 }
