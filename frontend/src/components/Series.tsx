a011df7f9cec8370d29819fad2d2aec70c5635f1
James <james.noung@gmail.com>
2025-02-16 07:24:48 +0700

    Refactor series

--- frontend/src/components/Series.tsx
+++ frontend/src/components/Series.tsx
@@ -20,29 +20,28 @@ export default function Series(){
    const randomSeries = getRandomSeries(series);
 
     return(
-        <div  className="series-section">
-            <h2>Series</h2>
-            <span onClick={()=>nav("series-page")}>See more</span>
-            <div className="series-scroll-container" >
+    <div  className="series-section">
+        <h2>Series</h2>
+        <span onClick={()=>nav("series-page")}>See more</span>
+        <div className="series-scroll-container" >
 
             <div className="series-grid">
 
-            { series && randomSeries.map((s) =>
-                     <div className="series-box"  key={s._id} onClick={()=>callNav(s._id)}> 
+                { series && randomSeries.map((s) =>
+                    <div className="series-box"  key={s._id} onClick={()=>callNav(s._id)}> 
                         <img src={s.poster_url} />
                         <div>
-                          <div className="series-text">
-                            <h3>{s.title}</h3>
-                            <span>{s.description}</span>
-                            <ul>
-                              {s.genres.map((g) => (
-                                <li key={g._id}>{g.name}</li>
-                              ))}
-                            </ul>
-                          </div>
+                            <div className="series-text">
+                                <h3>{s.title}</h3>
+                                <span>{s.description}</span>
+                                <ul>
+                                {s.genres.map((g) => (<li key={g._id}>{g.name}</li>))}
+                                </ul>
+                            </div>
                         </div>
                     </div>
-           )}
-           </div>
-        </div> 
+                )}
+            </div>
+        </div>
+    </div> 
 )}
