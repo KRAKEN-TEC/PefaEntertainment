d0453a22fce682fff9ad8a7777079985785dd52c
ChristianChann <141214253+ChristianChann@users.noreply.github.com>
2025-02-19 01:06:59 +0700

    Resfactor - NavBar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,4 @@
 import "./index.css";
-import { Provider } from "./components/ui/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
@@ -8,8 +7,6 @@ import router from "./router/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <RouterProvider router={router} />
-    </Provider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
