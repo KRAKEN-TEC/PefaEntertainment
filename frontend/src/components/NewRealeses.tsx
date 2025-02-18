9a1caf087c94fe32f76b1b59de5f4de8f9336121
Fezi-x <tzayamin@gmail.com>
2025-02-18 14:49:58 +0700

    css refactor

--- frontend/src/components/NewRealeses.tsx
+++ frontend/src/components/NewRealeses.tsx
@@ -1,36 +1,44 @@
-import { useMovie } from '@/hooks/useMovie'
-import './CSS/NewRealses.css'
-import useNavDetail from '@/hooks/useNavDetail';
+import { useMovie } from "@/hooks/useMovie";
+import "./CSS/NewRealses.css";
+import useNavDetail from "@/hooks/useNavDetail";
 
-export default function NewRealeses(){
-   const {data: newRealses } = useMovie();
-   const {callNav} = useNavDetail();
+export default function NewRealeses() {
+  const { data: newRealses } = useMovie();
+  const { callNav } = useNavDetail();
 
-    return(
-        <div  className="NR-movie-section">
-            <h2>New Realsese</h2>
-            <div className="NR-scroll-container" >
-
-            <div className="NR-movie-grid">
-
-            { newRealses && newRealses.map((newRealse) =>
-                     <div className="NR-movie-box"  key={newRealse._id} onClick={()=>callNav(newRealse._id)} > 
-                        <img src={newRealse.poster_url} />
-                        <div>
-                            <h3>{newRealse.title}</h3>
-                            <span>{newRealse.description}</span>
-                            <ul>
-                                    {newRealse.genres.map((genre)=>(
-                                        <li key={genre._id}>{genre.name}</li>
-                                    ))}
-                            </ul> 
-                        </div>
-
-                   </div>
-           )}
-           </div>
-                 </div> 
-            
+  return (
+    <div className="NR-movie-section">
+      <h2>New Realsese</h2>
+      <div className="NR-scroll-container">
+        <div className="NR-movie-grid">
+          {newRealses &&
+            newRealses.map((newRealse) => (
+              <div
+                style={{
+                  backgroundImage: `url(${newRealse.poster_url})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                  height: "500px",
+                }}
+                className="NR-movie-box"
+                key={newRealse._id}
+                onClick={() => callNav(newRealse._id)}
+              >
+                {/* <img src={newRealse.poster_url} /> */}
+                <div>
+                  <h3>{newRealse.title}</h3>
+                  <span>{newRealse.description}</span>
+                  <ul>
+                    {newRealse.genres.map((genre) => (
+                      <li key={genre._id}>{genre.name}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/SeriesPage.tsx
+++ frontend/src/pages/SeriesPage.tsx
@@ -1,40 +1,43 @@
-import { FetchMovie, useMovie } from '@/hooks/useMovie'
-import './CSS/SeriesPage.css'
-import useNavDetail from '@/hooks/useNavDetail';
+import { FetchMovie, useMovie } from "@/hooks/useMovie";
+import "./CSS/SeriesPage.css";
+import useNavDetail from "@/hooks/useNavDetail";
 
 export default function SeriesPage() {
-    const { data: series } = useMovie();
-    const { callNavForSeriesPage } = useNavDetail();
+  const { data: series } = useMovie();
+  const { callNavForSeriesPage } = useNavDetail();
 
-    const navigate = () => {
-        console.log("Hello")
-    }
+  const navigate = () => {
+    console.log("Hello");
+  };
 
-    return (
-        <div className="SP-section">
-            <h2>Series</h2>
-            <div className="SP-scroll-container" >
-
-                <div className="SP-grid">
-
-                    {series && series.filter((s) => s.isSerie).map((s) =>
-                        <div className="SP-box" key={s._id} onClick={() => callNavForSeriesPage(s._id)}>
-                            <img src={s.poster_url} />
-                            <div>
-                                <h3>{s.title}</h3>
-                                <span>{s.description}</span>
-                                <ul>
-                                    {s.genres.map((g) => (
-                                        <li key={g._id}>{g.name}</li>
-                                    ))}
-                                </ul>
-                            </div>
-
-                        </div>
-                    )}
+  return (
+    <div className="SP-section">
+      <h2>Series</h2>
+      <div className="SP-scroll-container">
+        <div className="SP-grid">
+          {series &&
+            series
+              .filter((s) => s.isSerie)
+              .map((s) => (
+                <div
+                  className="SP-box"
+                  key={s._id}
+                  onClick={() => callNavForSeriesPage(s._id)}
+                >
+                  <img src={s.poster_url} />
+                  <div className="SP-text">
+                    <h3>{s.title}</h3>
+                    <span>{s.description}</span>
+                    <ul>
+                      {s.genres.map((g) => (
+                        <li key={g._id}>{g.name}</li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
-            </div>
-
+              ))}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
