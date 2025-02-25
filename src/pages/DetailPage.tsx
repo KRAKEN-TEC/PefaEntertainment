a182f441661ac18ba8918910473cfa0a3f4c0887
Fezi-x <tzayamin@gmail.com>
2025-02-25 22:54:00 +0700

    nav bar refactor

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -39,7 +39,7 @@ export default function DetailPage() {
             backgroundImage: `url(${movieData?.poster_url})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
-            width: "97%",
+            width: "100%",
             height: "500px",
           }}
         >
