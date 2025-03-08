3a69198d5ac5da62dae043441479cf0e0eb2eb85
ChristianChann <141214253+ChristianChann@users.noreply.github.com>
2025-03-09 01:08:37 +0700

    hehe

--- src/helper/VideoPlayer.tsx
+++ src/helper/VideoPlayer.tsx
@@ -6,8 +6,8 @@ import Player from "video.js/dist/types/player";
 // GUIDE =>>> https://videojs.com/guides/react/
 
 interface Props {
-  posterUrl: string,
-  videoUrl: string,
+  posterUrl: string;
+  videoUrl: string;
   onReady?: (player: Player) => void;
 }
 
@@ -19,9 +19,9 @@ const VideoPlayer = ({ posterUrl, videoUrl, onReady }: Props) => {
     // Make sure Video.js player is only initialized once
     // if playerRef.current doesn't have value, create player, reference the playerRef.current to that player
     if (!playerRef.current) {
-      // create videoElement and set it to the videoRef.current 
+      // create videoElement and set it to the videoRef.current
       const videoElement = document.createElement("video-js");
-      videoElement.classList.add('vjs-big-play-centered');
+      videoElement.classList.add("vjs-big-play-centered");
       if (videoRef.current) videoRef.current.appendChild(videoElement);
 
       const isHLS = videoUrl.endsWith(".m3u8"); // This checks if the URL is HLS
@@ -33,37 +33,46 @@ const VideoPlayer = ({ posterUrl, videoUrl, onReady }: Props) => {
         preload: "auto",
         poster: posterUrl,
         sources: isHLS
-          ? [{
-            src: videoUrl,
-            type: 'application/x-mpegURL' // HLS
-          }]
-          : [{
-            src: videoUrl,
-            type: 'video/mp4' // MP4 fallback
-          }]
+          ? [
+              {
+                src: videoUrl,
+                type: "application/x-mpegURL", // HLS
+              },
+            ]
+          : [
+              {
+                src: videoUrl,
+                type: "video/mp4", // MP4 fallback
+              },
+            ],
       };
 
       // create player
-      const player = playerRef.current = videojs(videoElement, videoJsOptions, () => {
-        videojs.log('player is ready');
-        onReady && onReady(player);
-      });
-
-    }
-    else {
+      const player = (playerRef.current = videojs(
+        videoElement,
+        videoJsOptions,
+        () => {
+          videojs.log("player is ready");
+          onReady && onReady(player);
+        }
+      ));
+    } else {
       // if playerRef.current has value
       const player = playerRef.current;
       // videoJsOptions
       player.autoplay(false);
       const isHLS = videoUrl.endsWith(".m3u8"); // Check if the URL is HLS
-      player.src(isHLS
-        ? [{ src: videoUrl, type: 'application/x-mpegURL' }] // Use HLS URL
-        : [{ src: videoUrl, type: 'video/mp4' }] // Use MP4 fallback
+      player.src(
+        isHLS
+          ? [{ src: videoUrl, type: "application/x-mpegURL" }] // Use HLS URL
+          : [{ src: videoUrl, type: "video/mp4" }] // Use MP4 fallback
       );
-      player.src([{
-        src: videoUrl,
-        type: 'video/mp4'
-      }]);
+      player.src([
+        {
+          src: videoUrl,
+          type: "video/mp4",
+        },
+      ]);
     }
   }, []);
 
@@ -78,7 +87,6 @@ const VideoPlayer = ({ posterUrl, videoUrl, onReady }: Props) => {
       }
     };
   }, [playerRef]);
-
 
   return (
     <div data-vjs-player>
--- src/pages/MoviePage/MoviesPage.tsx
+++ src/pages/MoviePage/MoviesPage.tsx
@@ -1,37 +1,39 @@
-import { useMovie } from "@/hooks/useMovie";
+import { useEffect, useRef, useState } from "react";
+import { FetchMovies, useMovie } from "@/hooks/useMovie";
 import "../CSS/MoviesPage.css";
 import useNavDetail from "@/hooks/useNavDetail";
-
-// Ko Oak Kar ၀င်မရေးရ
+import { useMovieStore } from "@/context/useMovieStore";
 
 export default function MoviesPage() {
-  const { data: movies } = useMovie();
+  const { movieQuery, setMovieQuery } = useMovieStore();
+  const { data: movies } = useMovie(movieQuery);
   const { navMovieDetail } = useNavDetail();
+  const lastMovieRef = useRef(null);
 
   return (
     <div className="MP-section">
       <h2>Movies</h2>
       <div className="MP-scroll-container">
         <div className="MP-grid">
-          {movies &&
-            movies.map((movie) => (
-              <div
-                className="MP-box"
-                key={movie._id}
-                onClick={() => navMovieDetail(`${movie.slug}`)}
-              >
-                <img src={movie.poster_url} />
-                <div className="mp-text">
-                  <h3>{movie.title}</h3>
-                  <span>{movie.description}</span>
-                  <ul>
-                    {movie.genres.map((genre) => (
-                      <li key={genre._id}>{genre.name}</li>
-                    ))}
-                  </ul>
-                </div>
+          {preMovies.map((movie, index) => (
+            <div
+              ref={index === preMovies.length - 1 ? lastMovieRef : null}
+              className="MP-box"
+              key={movie._id}
+              onClick={() => navMovieDetail(`${movie.slug}`)}
+            >
+              <img src={movie.poster_url} />
+              <div className="mp-text">
+                <h3>{movie.title}</h3>
+                <span>{movie.description}</span>
+                <ul>
+                  {movie.genres.map((genre) => (
+                    <li key={genre._id}>{genre.name}</li>
+                  ))}
+                </ul>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
