9e66393f27da7f75c483cf77a59e2b6b997cb36d
James <james.noung@gmail.com>
2025-03-11 21:17:09 +0700

    activating search bar

--- src/components/Series/Series.tsx
+++ src/components/Series/Series.tsx
@@ -2,11 +2,13 @@ import { FetchSeries, useSeries } from "@/hooks/useSerie";
 import "../CSS/Series.css";
 import useNavDetail from "@/hooks/useNavDetail";
 import { genreLi } from "../global/genreLi";
+import { useSerieStore } from "@/context/useSerieStore";
 
 // Ko Oak Kar ၀င်မရေးရ
 
 export default function Series() {
-  const { data: series } = useSeries();
+  const { serieQuery } = useSerieStore();
+  const { data: series } = useSeries(serieQuery);
   const { nav, navSerieDetail } = useNavDetail();
 
   const getRandomSeries = (seriesStore: FetchSeries[], count = 3) => {
