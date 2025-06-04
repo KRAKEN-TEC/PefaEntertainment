c7bb6395df9d65e30cb826e9d6a65c09f200db78
James <james.noung@gmail.com>
2025-06-05 02:23:06 +0700

    update VideoPlayer logic

--- src/helper/VideoPlayer.tsx
+++ src/helper/VideoPlayer.tsx
@@ -1,92 +1,73 @@
-import React from "react";
+import { useEffect, useRef } from "react";
 import videojs from "video.js";
-import "video.js/dist/video-js.css";
-import "../components/CSS/WatchMovie.css"
 import Player from "video.js/dist/types/player";
+
+import "video.js/dist/video-js.css";
 
 // GUIDE =>>> https://videojs.com/guides/react/
 
 interface Props {
+  id: string;
   posterUrl: string;
   videoUrl: string;
   onReady?: (player: Player) => void;
 }
 
-const VideoPlayer = ({ posterUrl, videoUrl, onReady }: Props) => {
-  const videoRef = React.useRef<HTMLDivElement | null>(null);
-  const playerRef = React.useRef<Player | null>(null);
+const VideoPlayer = ({ id, posterUrl, videoUrl, onReady }: Props) => {
+  const videoRef = useRef<HTMLDivElement | null>(null);
+  const playerRef = useRef<Player | null>(null);
 
-  React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
-    // if playerRef.current doesn't have value, create player, reference the playerRef.current to that player
-    if (!playerRef.current) {
-      // create videoElement and set it to the videoRef.current
-      const videoElement = document.createElement("video-js");
-      videoElement.classList.add("vjs-big-play-centered");
-      if (videoRef.current) videoRef.current.appendChild(videoElement);
+  useEffect(() => {
+    const videoElement = document.createElement("video-js");
+    videoElement.classList.add("vjs-big-play-centered");
+    videoElement.setAttribute("id", id);
+    if (videoRef.current) videoRef.current.appendChild(videoElement);
 
-      const isHLS = videoUrl.endsWith(".m3u8"); // This checks if the URL is HLS
-      const videoJsOptions = {
-        autoplay: false,
-        controls: true,
-        responsive: true,
-        fluid: true,
-        preload: "auto",
-        poster: posterUrl,
-        sources: isHLS
-          ? [
-            {
-              src: videoUrl,
-              type: "application/x-mpegURL", // HLS
-            },
-          ]
-          : [
-            {
-              src: videoUrl,
-              type: "video/mp4", // MP4 fallback
-            },
-          ],
-      };
+    const isHLS = videoUrl.endsWith(".m3u8"); // This checks if the URL is HLS
+    const videoJsOptions = {
+      autoplay: false,
+      controls: true,
+      responsive: true,
+      fluid: true,
+      preload: "auto",
+      poster: posterUrl,
+      sources: isHLS
+        ? [
+          {
+            src: videoUrl,
+            type: "application/x-mpegURL", // HLS
+          },
+        ]
+        : [
+          {
+            src: videoUrl,
+            type: "video/mp4", // MP4 fallback
+          },
+        ],
+    };
 
-      // create player
-      const player = (playerRef.current = videojs(videoElement, videoJsOptions, () => {
-        videojs.log("player is ready");
-        onReady && onReady(player);
+    // create player
+    const player = videojs(videoElement, videoJsOptions, () => {
+      videojs.log("player is ready");
+      onReady && onReady(player);
 
-        // // Enable plugins here
-        // player.ads(); // Initialize ads plugin
+    });
+    // // Enable plugins here
+    // player.ads(); // Initialize ads plugin
 
-        // player.vast({
-        //   url: "https://s.magsrv.com/v1/vast.php?idzone=2366423", // replace with your actual ID zone
-        //   playAdAlways: true,
-        //   adCancelTimeout: 5000,
-        //   width: 640,
-        //   height: 360,
-        // });
-      }
-      ));
-    } else {
-      // if playerRef.current has value
-      const player = playerRef.current;
-      // videoJsOptions
-      player.autoplay(false);
-      const isHLS = videoUrl.endsWith(".m3u8"); // Check if the URL is HLS
-      player.src(
-        isHLS
-          ? [{ src: videoUrl, type: "application/x-mpegURL" }] // Use HLS URL
-          : [{ src: videoUrl, type: "video/mp4" }] // Use MP4 fallback
-      );
-      player.src([
-        {
-          src: videoUrl,
-          type: "video/mp4",
-        },
-      ]);
-    }
+    // player.vast({
+    //   url: "https://s.magsrv.com/v1/vast.php?idzone=2366423", // replace with your actual ID zone
+    //   playAdAlways: true,
+    //   adCancelTimeout: 5000,
+    //   width: 640,
+    //   height: 360,
+    // });
+
+    playerRef.current = player;
   }, []);
 
   // Dispose the Video.js player when the functional component unmounts
-  React.useEffect(() => {
+  useEffect(() => {
     const player = playerRef.current;
 
     return () => {
